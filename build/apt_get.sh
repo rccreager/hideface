c99769fa544f82dffc6539f5b034652a9f0b70d1
Rachael Creager <rachaelcreager@gmail.com>
2020-01-21 21:53:50 +0000

    adding apt_get script

---
+++ build/apt_get.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get -y install build-essential cmake pkg-config
+sudo apt-get -y install libx11-dev libatlas-base-dev
+sudo apt-get -y install libgtk-3-dev libboost-python-dev
+sudo apt-get -y install python3-venv
+
---
+++ test.py
@@ -0,0 +1,3 @@
+import sys
+import dlib
+from skimage import io
