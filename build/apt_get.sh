08bca521c6e846dc8b5595c0dbe2939d23e4c2a4
root <root@ip-172-31-4-117.us-west-2.compute.internal>
2020-01-21 22:22:32 +0000

    adding test script, more apt-get tools

--- build/apt_get.sh
+++ build/apt_get.sh
@@ -3,4 +3,4 @@ sudo apt-get -y install build-essential cmake pkg-config
 sudo apt-get -y install libx11-dev libatlas-base-dev
 sudo apt-get -y install libgtk-3-dev libboost-python-dev
 sudo apt-get -y install python3-venv
-
+sudo apt-get -y install s3fs
--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 import sys
 import dlib
 from skimage import io
+
+file_name = "/s3mnt/lfw/George_W_Bush/George_W_Bush_0001.jpg"
+image = io.imread(file_name)
+face_detector = dlib.get_frontal_face_detector()
+win = dlib.image_window()
