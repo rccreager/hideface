35e972a40673e590f81a22a3960182a1b2d95bf7
Rachael Creager <rachaelcreager@gmail.com>
2020-01-21 23:47:35 +0000

    updated build readme, test script

--- build/README.md
+++ build/README.md
@@ -1,8 +1,9 @@
 # Setting up your environment
 
 These directions assume you are working from a fresh Ubuntu 18.04 EC2 instance.
+I'm including some optional directions for X11 forwarding; if you are working on a local machine, this isn't needed.
 
-## Setting up X11 forwarding
+## Setting up X11 forwarding (optional)
 When you ssh into your instance, make sure to enable the **-Y** flag.
 Next, edit your ssh_config file:
 
@@ -19,3 +20,14 @@ Finally, install some X11 tools and test that your forwarding works with **xcloc
     xclock
 
 If you set it up right, a little clock will appear on your screen!
+
+## Installing apt-get packages
+The script **apt_get.sh** includes sudo commands to install all the necessary systemwide tools.
+After that, you can use **requirements.txt** to install the needed python packages to your pip3.
+I would recommend setting up a virtual environment first (the second and third lines):
+
+    source build/apt_get.sh
+    python3 -m venv build/my_first_env
+    source build/my_first_env/bin/activate
+    pip3 install build/requirements.txt
+
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+#this is a copy of Adam Geitgey's HoG script
+#https://gist.github.com/ageitgey/1c1cb1c60ace321868f7410d48c228e1
+#I am using it for testing purposes!
+
 import sys
 import dlib
 from skimage import io
@@ -6,3 +10,20 @@ file_name = "/s3mnt/wider_face/51--Dresses/51_Dresses_wearingdress_51_1002.jpg"
 image = io.imread(file_name)
 face_detector = dlib.get_frontal_face_detector()
 win = dlib.image_window()
+detected_faces = face_detector(image, 1)
+print("I found {} faces in the file {}".format(len(detected_faces), file_name))
+win.set_image(image)
+
+# Loop through each face we found in the image
+for i, face_rect in enumerate(detected_faces):
+
+	# Detected faces are returned as an object with the coordinates 
+	# of the top, left, right and bottom edges
+	print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
+
+	# Draw a box around each face we found
+	win.add_overlay(face_rect)
+	        
+# Wait until the user hits <enter> to close the window	 
+
+dlib.hit_enter_to_continue()
