bacd343d91c83f7468a3a8e8142654667549ee6d
Rachael Creager <rachaelcreager@gmail.com>
2020-01-24 00:37:13 +0000

    cleaning up

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-#this is a copy of Adam Geitgey's HoG script
-#https://gist.github.com/ageitgey/1c1cb1c60ace321868f7410d48c228e1
-#I am using it for testing purposes!
+# this is an explansion of a fork of Adam Geitgey's HoG script
+# https://gist.github.com/ageitgey/1c1cb1c60ace321868f7410d48c228e1
 
 import sys
 import dlib
@@ -12,31 +11,17 @@ file_name = "51_Dresses_wearingdress_51_53.jpg"
 file_path = "/wider-face/WIDER_train/images/51--Dresses/"
 
 true_box_list = tools.get_ground_truth('51_53', '/wider-face/wider_face_split/wider_face_train_bbx_gt.txt')
-#print(true_box_list)
 
 image = io.imread(file_path + file_name)
 face_detector = dlib.get_frontal_face_detector()
 detected_faces = face_detector(image, 1)
 found_box_list = [tools.FaceBox(face.left(), face.top(), face.right()-face.left(), face.bottom()-face.top()) for face in detected_faces]
-#print(found_box_list)
 
 best_matches = tools.get_best_matches(true_box_list, found_box_list)
 print(*best_matches,sep="\n")
 
-#null_box = tools.FaceBox(0,0,0,0)
-#best_matches = [[found_box,null_box] for found_box in found_box_list]
-#
-#for box_pair in best_matches:
-#    for true_box in true_box_list:
-#        old_iou = box_pair[0].iou(box_pair[1])
-#        new_iou = box_pair[0].iou(true_box)
-#        if (new_iou > old_iou):
-#            box_pair[1] = true_box
-#print(best_matches)
-
 win = dlib.image_window()
 win.set_image(image)
-# Loop through each face we found in the image
 for i, face_rect in enumerate(detected_faces):
     # Draw a box around each face we found
     win.add_overlay(face_rect)
