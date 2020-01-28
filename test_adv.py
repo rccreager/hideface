e9f74e9ebd1e84474e8199836c8841db162565e9
Ubuntu <ubuntu@ip-172-31-22-190.us-west-2.compute.internal>
2020-01-28 19:07:52 +0000

    updating for fresh build

--- test_adv.py
+++ test_adv.py
@@ -5,15 +5,16 @@ import cleverhans
 import cv2
 
 face_cascade = cv2.CascadeClassifier('/home/ubuntu/face-detection-adversarial-attack/data/haar_xml/haarcascade_frontalface_default.xml')
-print(type(face_cascade))
+#print(type(face_cascade))
 #fgsm = FastGradientMethod(face_cascade)
 
 
 
-file_path = "/wider-face/WIDER_train/images/51--Dresses/51_Dresses_wearingdress_51_10.jpg"
-img = cv2.imread(file_path)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+file_path = "/s3mnt/WIDER_train/images/51--Dresses/51_Dresses_wearingdress_51_10.jpg"
+img = io.imread(file_path)
+#img = cv2.imread(file_path)
+#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
 
 #adv = fgsm.generate(image, clip_min=-1., clip_max=1.)
--- test_wider_face_hog.py
+++ test_wider_face_hog.py
@@ -5,16 +5,17 @@ import sys
 import dlib
 import re
 import random
-from os import listdir
-from os.path import join
+import os
+#from os.path import join
 from skimage import io 
 from matplotlib import pyplot as plt 
-sys.path.insert(0, '/home/ubuntu/face-detection-adversarial-attack/Insight_Project_Framework')
-from Insight_Project_Framework import tools
+sys.path.insert(0, '/home/ubuntu/face-detection-adversarial-attack/hideface')
+from hideface import tools
 
-file_path = "/wider-face/WIDER_train/images/51--Dresses/"
-files = [join(file_path, file_name) for file_name in listdir(file_path)] 
-#files = random.sample(files,10)
+file_path = "/s3mnt/WIDER_train/images/51--Dresses/"
+files = [os.path.join(file_path, file_name) for file_name in os.listdir(file_path)] 
+n = 10
+files = random.sample(files,n)
 
 iou = [] # use this to store IoU for all found faces
 
@@ -26,7 +27,7 @@ for file_name in files:
 
     face_detector = dlib.get_frontal_face_detector()
 
-    true_box_list = tools.get_ground_truth_boxes(file_num, '/wider-face/wider_face_split/wider_face_train_bbx_gt.txt')
+    true_box_list = tools.get_ground_truth_boxes(file_num, '/s3mnt/wider_face_split/wider_face_train_bbx_gt.txt')
     if (len(true_box_list) != 1): continue
     if (true_box_list[0].quality != tools.TruthBoxQuality(0,0,0,0,0,0)): continue
     
@@ -35,8 +36,10 @@ for file_name in files:
     for match in best_matches:
         iou.append(match.true_box.iou(match.found_box))
     tools.draw_boxes(image, true_box_list, found_box_list)
+    if not (os.path.isdir('data/boxed_output/')): os.mkdir('data/boxed_output/')
     io.imsave('data/boxed_output/boxed_'+file_num+'.jpg', image)
 
 plt.hist(iou, bins = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]) 
 plt.title("IoU Histogram") 
+if not (os.path.isdir('data/histos/')): os.mkdir('data/histos/')
 plt.savefig("data/histos/iou.png")    
