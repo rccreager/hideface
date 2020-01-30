b622bc57940579e2c264f6165f9a2167134e3953
Ubuntu <ubuntu@ip-172-31-10-123.us-west-2.compute.internal>
2020-01-30 01:17:25 +0000

    reconfigured utils file

--- hideface/utils.py
+++ hideface/utils.py
@@ -1,77 +1,65 @@
-import os
 import re
-import random
-import numpy as np
 from skimage import io
 from hideface import tools
 
-class Result:
+class ImageLabels:
     """
-    A class for storing the results of labeling and attacking images 
+    A class for storing true FaceBox and found FaceBox information for a given image 
 
     Attributes:
-        original_img_name: the name of the original image 
-        true_box_list: a list of ground truth FaceBox objects (can be empty)
-        original_found_box_dict: a dictionary of recognizer names and found box lists prior to attack 
-        attacked_found_box_dict: a nested dictionary of found boxes for each recognizer after each attack
+        img_name: the path to the image
+        true_box_list: a list of ground truth FaceBox objects
+        found_box_dict: a dictionary of recognizer names and found box lists prior to attack 
+        attack_name: if an attack has been applied, a string naming the attack
     """
-    def __init__(self, original_img_name, true_box_list, original_found_box_dict={}, attacked_found_box_dict={}):
-        self.original_img_name = original_img_name
+    def __init__(self, img_name, true_box_list=[], found_box_dict={}, attack_name=None):
+        self.img_name = img_name
         self.true_box_list = true_box_list
-        self.original_found_box_dict = original_found_box_dict
-        self.attacked_found_box_dict = attacked_found_box_dict 
+        self.found_box_dict = found_box_dict
+        self.attack_name = attack_name
     def __str__(self):
-        return "(WF Img Name: \n{} \nTruth Box List: \n{} \nOriginal Found Box Dict: \n{} \nAttacked Found Box Dict: \n{})".format(self.original_img_name, self.true_box_list, self.original_found_box_dict, self.attacked_found_box_dict)
+        return "(Img Name: \n{} \nTruth Box List: \n{} \nFound Box Dict: \n{} \nAttack Applied: \n{})".format(self.img_name, self.true_box_list, self.found_box_dict, self.attack_name)
     def __repr__(self):
         return str(self)
+    def add_recognizer_labels(self,recognizer_dict):
+        """
+        For each dictionary entry {recognizer_name:recognizer}, find the FaceBox list for that recognizer and modify the class attribute
+        """
+        print("adding facial recognition for image: " + self.img_name)
+        found_box_dict = {}
+        for recognizer_name, recognizer in recognizer_dict.items():
+            found_box_list = tools.get_found_boxes(self.img_name,recognizer)
+            found_box_dict.update( {recognizer_name : found_box_list} )
+        self.found_box_dict = found_box_dict
+        return self
+    def add_truth_labels(self,truth_file):
+        """
+        Given a WiderFace truth file, find the ground truth FaceBox for the image and modify the class attribute
+        """
+        print("adding truth labels for image: " + self.img_name)
+        true_box_list=[]
+        if(truth_file != None): true_box_list = tools.get_ground_truth_boxes(self.img_name,truth_file)
+        self.true_box_list = true_box_list
+        return self
 
-def create_labeled_image(result, output_dir):
+#def draw_and_return_attacked_image_labels(original_img_name, )
+
+def draw_labeled_image(image_labels, output_dir):
     """
-    Create labeled images for images in a given directory
-    
+    For a given ImageLabels object, write a copy of the image with FaceBoxes drawn to output_dir
+
     Attributes:
-        result: a Result object for a given image
+        image_labels: an ImageLabel object which we want to visualize
         output_dir: a string giving the path to the desired output directory
     Returns:
         None. Just saves image outputs to output_dir 
     """
-    image = io.imread(result.original_img_name)
-    tools.draw_boxes(image,result.true_box_list, (0,0,255))
-    if (len(result.original_found_box_dict) == 0): 
-        io.imsave(output_dir + '/boxed_truth_'+file_num+'.jpg', image)
-    for recognizer_name, found_box_list in result.original_found_box_dict.items():
+    image = io.imread(image_labels.img_name)
+    tools.draw_boxes(image,image_labels.true_box_list, (0,0,255))
+    if (len(image_labels.found_box_dict) == 0): 
+        io.imsave(output_dir + '/boxed_no_recognizer_'+file_num+'.jpg', image)
+    for recognizer_name, found_box_list in image_labels.found_box_dict.items():
         tools.draw_boxes(image, found_box_list, (255,0,0))
-        file_num = re.findall(r"[0-9]+_[0-9]+\.jpg", result.original_img_name)[0][:-4]
+        file_num = re.findall(r"[0-9]+_[0-9]+\.jpg", image_labels.img_name)[0][:-4]
         io.imsave(output_dir + '/boxed_'+recognizer_name+'_'+file_num+'.jpg', image)
 
-def get_labels(img_input_dir, num_imgs=None, truth_file=None, recognizer_dict={}, csv_output=None):
-    """
-    Given a directory of images and (possibly) a truth file and list of recognizers, return
-    a numpy array of Results objects
-
-    Args:
-        img_input_dir: path to the image directory
-        num_imgs: optionally set the number of images to check
-        truth_file: optional path to the truth file
-        recognizer_dict: optionally specify dictionary of recognizer names and recognizers
-        csv_output: optionally specify a path to write a CSV output file
-    Returns:
-        results: a list of Result objects
-    """
-    img_names = [img_input_dir + img_name for img_name in os.listdir(img_input_dir)]    
-    if (num_imgs != None): 
-        print('Number of Images to Label: ' + str(num_imgs))
-        img_names = random.sample(img_names,num_imgs)
-    results = np.empty(shape=(0))
-    for img_name in img_names:
-        print('Processing image: ' + img_name)
-        found_box_dict = {}
-        true_box_list=[]
-        if(truth_file != None): true_box_list = tools.get_ground_truth_boxes(img_name,truth_file)
-        for recognizer_name, recognizer in recognizer_dict.items(): 
-            found_box_list = tools.get_found_boxes(img_name,recognizer)
-            found_box_dict = {recognizer_name: found_box_list}
-        result = Result(img_name,np.asarray(true_box_list),found_box_dict)
-        results = np.append(results,result)
-    if (csv_output != None): numpy.savetxt(csv_output, results, delimiter=",")
-    return results
--- test_wider_face.py
+++ test_wider_face.py
@@ -1,8 +1,8 @@
 import dlib
 #import sys
 #import re
-#import random
-#import os
+import random
+import os
 #from skimage import io 
 #from matplotlib import pyplot as plt 
 from hideface import tools, utils
@@ -11,8 +11,13 @@ img_input_dir = "data/WIDER_train/images/51--Dresses/"
 truth_file = 'data/wider_face_split/wider_face_train_bbx_gt.txt'
 recognizer_dict = {'hog':dlib.get_frontal_face_detector()} 
 num_imgs = 5
-results = utils.get_labels(img_input_dir, num_imgs, truth_file, recognizer_dict)
-print(results[0])
-for result in results:
-    utils.create_labeled_image(result, 'data/boxed_output')
+img_names = random.sample([img_name for img_name in os.listdir(img_input_dir)],num_imgs)
+image_labels_list = [utils.ImageLabels(img_input_dir + img_name) for img_name in img_names]
+print(image_labels_list[0])
+image_labels_list = [image_labels.add_truth_labels(truth_file) for image_labels in image_labels_list]
+print(image_labels_list[0])
+image_labels_list = [image_labels.add_recognizer_labels(recognizer_dict) for image_labels in image_labels_list]
+print(image_labels_list[0])
+for image_labels in image_labels_list:
+    utils.draw_labeled_image(image_labels, 'data/boxed_output')
 
