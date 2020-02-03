46fae8cee99758a4dc5ca286057e93e9ad9e3162
Ubuntu <ubuntu@ip-172-31-10-123.us-west-2.compute.internal>
2020-02-03 20:02:47 +0000

    updated ImageLabels to use kwargs

--- hideface/utils.py
+++ hideface/utils.py
@@ -1,6 +1,6 @@
 import re
 import os
-#import tensorflow as tf
+import sys
 import numpy as np
 from skimage import io
 from hideface import tools
@@ -13,20 +13,24 @@ class ImageLabels:
         img_path: the path to the image
         true_box_list: a list of ground truth FaceBox objects
         found_box_dict: a dictionary of recognizer names and found box lists prior to attack 
-        noise_epsilon: epsilon value for noise applied to the image 
+        **kwargs: arguments for describing the attack applied (ex: noise_epsilon)
      
     Methods:
         add_recognizer_labels: given a recognizer_dict, set the found_box_dict and return
         add_truth_labels: given a truth file, set the truth_box_list and return
         add_all_labels: given a truth file and recognizer_dict, set relevant attributes and return
     """
-    def __init__(self, img_path, true_box_list=[], found_box_dict={}, noise_epsilon=0):
+    def __init__(self, img_path, true_box_list=[], found_box_dict={}, **kwargs):
         self.img_path = img_path
         self.true_box_list = true_box_list
         self.found_box_dict = found_box_dict
-        self.noise_epsilon = noise_epsilon
+        self.__dict__.update(kwargs)
     def __str__(self):
-        return "(Img Path: \n{} \nTruth Box List: \n{} \nFound Box Dict: \n{} \nNoise Epsilon: \n{})".format(self.img_path, self.true_box_list, self.found_box_dict, self.noise_epsilon)
+        main_str = "(Img Path: \n{} \nTruth Box List: \n{} \nFound Box Dict: \n{}\n".format(self.img_path, self.true_box_list, self.found_box_dict)
+        kwargs_str = "Attack Information:"
+        for key, item in self.kwargs.items(): kwargs_str += "\n{}: {}".format(key, item)
+        kwargs_str += ")"
+        return main_str + kwargs_str
     def __repr__(self):
         return str(self)
     def add_recognizer_labels(self,recognizer_dict):
@@ -38,6 +42,7 @@ class ImageLabels:
         Returns: 
             self
         """
+        if (not os.path.isfile(self.img_path)): sys.exit('Attempted to add recognizer labels to bad image: ' + self.img_path)
         found_box_dict = {}
         for recognizer_name, recognizer in recognizer_dict.items():
             found_box_list = tools.get_found_boxes(self.img_path,recognizer)
@@ -53,6 +58,7 @@ class ImageLabels:
         Returns:
             self    
         """
+        if (not os.path.isfile(self.img_path)): sys.exit('Attempted to add truth labels to bad image: ' + self.img_path)
         true_box_list=[]
         img_num = re.findall(r"[0-9]+_[0-9]+\.jpg", str(self.img_path))[0][:-4] 
         if(truth_file != None): true_box_list = tools.get_ground_truth_boxes(img_num,truth_file)
@@ -72,7 +78,7 @@ class ImageLabels:
         self.add_truth_labels(truth_file)
         return self
 
-def draw_and_return_noisy_image_labels(original_img_path, max_noise_epsilon, output_img_dir):
+def draw_and_return_noisy_image_labels(original_img_path, max_noise_epsilon, output_dir):
     """
     Given a path to an image, this function creates a new image with normally-distributed noise
     randomly applied and returns an ImageLabels object for the newly created image
@@ -80,13 +86,14 @@ def draw_and_return_noisy_image_labels(original_img_path, max_noise_epsilon, out
     Args:
         original_img_path: the path of the image you want to attack
         max_noise_epsilon: an integer giving the maximum variation (out of 255)
-        output_img_dir: the directory where you'd like to store the attacked image
+        output_dir: the directory where you'd like to store the attacked image
     Returns:
         an ImageLabels object for the noisy file without any FaceBox labels set
     """
+    if (not os.path.isfile(original_img_path)): sys.exit("Bad input image path: " + original_img_path)
+    if (not os.path.exists(output_dir)): os.makedirs(output_dir)
     eps = max_noise_epsilon / 255.0
     file_num = re.findall(r"[0-9]+_[0-9]+\.jpg", str(original_img_path))[0][:-4]
-    #print("Making Noisy (max_eps: "+str(max_noise_epsilon)+") Image: " + file_num)
     image = io.imread(original_img_path)
     pixels = np.copy(np.asarray(image).astype(np.float32))
     pixels /= 255.0
@@ -95,26 +102,31 @@ def draw_and_return_noisy_image_labels(original_img_path, max_noise_epsilon, out
     noisy_pixels *= 255
     noisy_pixels = np.asarray(noisy_pixels).astype(np.uint8)
     file_name = 'noisy_maxeps'+str(max_noise_epsilon)+'_'+file_num+'.jpg'
-    noisy_file_path = os.path.join(output_img_dir,file_name)
+    noisy_file_path = os.path.join(output_dir,file_name)
     io.imsave(noisy_file_path,noisy_pixels)
-    return ImageLabels(noisy_file_path,noise_epsilon=max_noise_epsilon)
+    return ImageLabels(noisy_file_path,kwargs={'noise_epsilon':max_noise_epsilon})
 
-def draw_labeled_image(image_labels, output_dir):
+def draw_labeled_image(image_labels, output_dir, attack_str=""):
     """
     For a given ImageLabels object, write a copy of the image with FaceBoxes drawn to output_dir
 
     Args:
         image_labels: an ImageLabel object which we want to visualize
         output_dir: a string giving the path to the desired output directory
+        attack_str: an optional string describing the attack applied to this image
     """
+    if (not os.path.exists(output_dir)): os.makedirs(output_dir)
     image = io.imread(image_labels.img_path)
     tools.draw_boxes(image,image_labels.true_box_list, (0,0,255))
     file_num = re.findall(r"[0-9]+_[0-9]+\.jpg", image_labels.img_path)[0][:-4]
     if (len(image_labels.found_box_dict) == 0): 
         name_str = 'no_recognizer'
-        if (len(image_labels.true_box_list) == 0): name_str = name_str + '_no_truth'
-        io.imsave(output_dir + '/boxed_'+name_str+'_eps'+str(image_labels.noise_epsilon)+'_'+file_num+'.jpg', image)
+        if (len(image_labels.true_box_list) == 0): name_str += '_no_truth'
+        if (attack_str != ""): name_str = name_str + '_' + attack_str + '_'
+        io.imsave(output_dir + '/boxed_'+name_str+file_num+'.jpg', image)
     for recognizer_name, found_box_list in image_labels.found_box_dict.items():
         tools.draw_boxes(image, found_box_list, (255,0,0))
-        io.imsave(output_dir + '/boxed_'+recognizer_name+'_eps'+str(image_labels.noise_epsilon)+'_'+file_num+'.jpg', image)
+        name_str = ''
+        if (attack_str != ""): name_str = name_str + '_' + attack_str + '_'
+        io.imsave(output_dir + '/boxed_'+ name_str + recognizer_name+'_'+file_num+'.jpg', image)
 
--- test_wider_face.py
+++ test_wider_face.py
@@ -4,55 +4,45 @@ import os
 import re
 from hideface import tools, utils
 
-img_input_dir = "data/WIDER_train/images/51--Dresses/"
+img_input_dir = 'data/WIDER_train/images/51--Dresses/'
 truth_file = 'data/wider_face_split/wider_face_train_bbx_gt.txt'
 recognizer_dict = {'hog':dlib.get_frontal_face_detector()} 
 #num_imgs = len(os.listdir(img_input_dir))
-#img_paths = random.sample([img_input_dir+img_name for img_name in os.listdir(img_input_dir)],num_imgs)
-img_paths = [img_input_dir+img_name for img_name in os.listdir(img_input_dir)]
-max_good_image_count = 999
+num_imgs = 20
+img_paths = random.sample([img_input_dir+img_name for img_name in os.listdir(img_input_dir)],num_imgs)
+output_dir = 'data/test_guards'
+max_epsilon_value = 240
+epsilon_delta = 16
 
-good_image_count = 0
+if (os.path.exists(output_dir) and len(os.listdir(output_dir)) != 0):
+    user_input = input('Output directory not empty: ' + output_dir + '\nProceed? [y/N]')
+    if (user_input != 'y'): sys.exit('Exiting')
+
 fail_count = 0
 for recognizer_name, _ in recognizer_dict.items():
     for img_path in img_paths:
-        if (good_image_count > max_good_image_count): 
-            print("Found " + str(max_good_image_count) + " images")
-            print("Failed to beat " + str(fail_count) + " images")
-            break
-        img_num = re.findall(r"[0-9]+_[0-9]+\.jpg", str(img_path))[0][:-4]
+        img_num = re.findall(r'[0-9]+_[0-9]+\.jpg', str(img_path))[0][:-4]
         image_labels = utils.ImageLabels(img_path).add_all_labels(truth_file, recognizer_dict)
-        
-        print("Testing Image: " + img_num)
-        if (len(image_labels.true_box_list) > 1): 
-            print("Found >1 true faces, skipping")
-            continue
-        if (image_labels.true_box_list[0].quality != tools.TruthBoxQuality(0,0,0,0,0,0)):
-            print("True Face Quality too poor (" + str(image_labels.true_box_list[0].quality) + "), skipping")
-            continue 
-        if (len(image_labels.found_box_dict[recognizer_name]) == 0):
-            print("Could not find face prior to noise application, skipping")
-            continue
+        print('Testing Image: ' + img_num)
+        if (len(image_labels.true_box_list) > 1): print('Found >1 true faces, skipping'); continue
+        if (image_labels.true_box_list[0].quality != tools.TruthBoxQuality(0,0,0,0,0,0)): print('True Face Quality too poor (' + str(image_labels.true_box_list[0].quality) + '), skipping'); continue 
+        if (len(image_labels.found_box_dict[recognizer_name]) == 0): print('Could not find face prior to noise application, skipping'); continue
         
         epsilon = 16
-        noisy_image_labels = utils.draw_and_return_noisy_image_labels(img_path, epsilon, 'data/noisy_outputs').add_all_labels(truth_file, recognizer_dict)
-        while (epsilon < 240):
+        noisy_image_labels = utils.draw_and_return_noisy_image_labels(img_path, epsilon, output_dir).add_all_labels(truth_file, recognizer_dict)
+        while (epsilon < max_epsilon_value):
             if (len(noisy_image_labels.found_box_dict[recognizer_name]) == 0):
-                print("No boxes found with epsilon: " + str(epsilon))
-                utils.draw_labeled_image(image_labels, 'data/noisy_compare')
-                utils.draw_labeled_image(noisy_image_labels, 'data/noisy_compare')
-                good_image_count += 1
+                print('No boxes found with epsilon: ' + str(epsilon))
+                utils.draw_labeled_image(image_labels, output_dir)
                 break
             if (len(noisy_image_labels.found_box_dict[recognizer_name]) == 1):
                 if (image_labels.true_box_list[0].iou(noisy_image_labels.found_box_dict[recognizer_name][0]) < 0.2):
-                    print("Box IoU with truth < 0.2 with epsilon: " + str(epsilon))
-                    utils.draw_labeled_image(image_labels, 'data/noisy_compare')
-                    utils.draw_labeled_image(noisy_image_labels, 'data/noisy_compare')
-                    good_image_count += 1
+                    print('Box IoU with truth < 0.2 with epsilon: ' + str(epsilon))
+                    utils.draw_labeled_image(image_labels, output_dir)
                     break
-            epsilon += 16
-            #print('Trying noise epsilon: ' + str(epsilon))
-            noisy_image_labels = utils.draw_and_return_noisy_image_labels(img_path, epsilon, 'data/noisy_outputs').add_all_labels(truth_file, recognizer_dict)
-        else:
-            print("Noise attack failed")
-            fail_count += 1
+            #attack failed with given epsilon value, so delete old image and increase epsilon
+            os.remove(noisy_image_labels.img_path)
+            epsilon += epsilon_delta
+            noisy_image_labels = utils.draw_and_return_noisy_image_labels(img_path, epsilon, output_dir).add_all_labels(truth_file, recognizer_dict)
+        else: print('Noise attack failed'); fail_count += 1
+print('Failed to beat ' + str(fail_count) + ' images with max epsilon: ' + str(max_epsilon_value))
