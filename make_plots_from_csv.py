547bc6cb81ec7772fa8cd08b16a7138d0356c11b
Rachael Creager <rccreager@gmail.com>
2020-02-09 23:06:42 +0000

    pep8

--- make_plots_from_csv.py
+++ make_plots_from_csv.py
@@ -21,7 +21,10 @@ def draw_histo(df, col_name, x_start, bin_width, n_bins, title, output_dir, outp
         output_dir: the directory to write your output images to
         output_filename: the name you'd like to give the png output image
     """
-    plt = df[col_name].plot.hist(bins=[bin_width*(x+0.5)+x_start for x in np.arange(0,n_bins)], edgecolor='black', color='#88d498')
+    plt = df[col_name].plot.hist(
+            bins=[bin_width*(x+0.5)+x_start for x in np.arange(0,n_bins)], 
+            edgecolor='black', 
+            color='#88d498')
     plt.set_title(title)
     print(col_name + ' max: ' + str(df[col_name].max()) + ', min: ' + str(df[col_name].min()))
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0,3)) 
@@ -32,22 +35,45 @@ if __name__ == "__main__":
     performance_csv = 'data/10k_imgs_addnoise/performance_list.csv'
     output_dir = '/home/ubuntu/hideface'
     with open (performance_csv) as csv_file:
-        df = pd.read_csv(csv_file, dtype={'img_num':str,'epsilon':int,'img_size': int, 'true_box_size':int,'truth_iou_no_noise':float,'truth_iou_noise':float,'ssim':float})  
+        df = pd.read_csv(csv_file, 
+                dtype={
+                    'img_num':str,
+                    'epsilon':int,
+                    'img_size': int, 
+                    'true_box_size':int,
+                    'truth_iou_no_noise':float,
+                    'truth_iou_noise':float,
+                    'ssim':float})  
         print(df.head(10))
-        df_attacks = df[df['epsilon'] >= 0] # get rid of rows from failed attacks  
+        df = df[df['epsilon'] >= 0] # get rid of rows from failed attacks  
     
-        draw_histo(df_attacks, 'epsilon', -16, 16, 16, 'Epsilon Additive Noise Value to Beat HoG', output_dir, 'eps_hist.png')
-        draw_histo(df_attacks, 'img_size', 400000, 100000, 21, 'Image Size (Width x Height)', output_dir, 'img_size_hist.png') 
-        draw_histo(df_attacks, 'true_box_size', -15000, 15000, 25, 'True Face Box Size (Width x Height)', output_dir, 'true_box_size_hist.png') 
-        draw_histo(df_attacks, 'truth_iou_no_noise', -0.05, 0.05, 21, 'IoU between Found and True Box Prior to Attack', output_dir, 'truth_iou_no_noise.png')
-        draw_histo(df_attacks, 'truth_iou_noise', -0.05, 0.05, 21, 'IoU between Found and True Box After Noise Attack', output_dir, 'truth_iou_noise.png')
-        draw_histo(df_attacks, 'ssim', -0.05, 0.05, 21, 'Structural Similarity', output_dir, 'ssim.png')
+        eps_title = 'Epsilon Additive Noise Value to Beat HoG'
+        eps_filename = 'eps_hist.png'
+        draw_histo(df, 'epsilon', -16, 16, 16, eps_title, output_dir, eps_filename)
+        size_title = 'Image Size (Width x Height)'
+        size_filename = 'img_size_hist.png'
+        draw_histo(df, 'img_size', 400000, 100000, 21, size_title, output_dir, size_filename) 
+        box_title = 'True Face Box Size (Width x Height)'
+        box_filename = 'true_box_size_hist.png'
+        draw_histo(df, 'true_box_size', -15000, 15000, 25, box_title, output_dir, box_filename) 
+        iou_title = 'IoU between Found and True Box Prior to Attack'
+        iou_filename = 'truth_iou_no_noise.png'
+        draw_histo(df, 'truth_iou_no_noise', -0.05, 0.05, 21, iou_title, output_dir, iou_filename) 
+        iou_title = 'IoU between Found and True Box After Noise Attack'
+        iou_filename = 'truth_iou_noise.png'
+        draw_histo(df, 'truth_iou_noise', -0.05, 0.05, 21, iou_title, output_dir, iou_filename)
+        draw_histo(df, 'ssim', -0.05, 0.05, 21, 'Structural Similarity', output_dir, 'ssim.png')
 
         bin_width = 0.01
         x_start = -0.01
         n_bins = 31
-        true_box_size_frac_plt = (df_attacks['true_box_size']/df_attacks['img_size']).plot.hist(bins=[bin_width*(x+0.5)+x_start for x in np.arange(0,n_bins)], edgecolor='black', color='#88d498')
-        print('true_box_size_frac max: ' + str((df_attacks['true_box_size']/df_attacks['img_size']).max()) + ', min: ' + str((df_attacks['true_box_size']/df_attacks['img_size']).min()))
+        true_box_size_frac_plt = (df['true_box_size']/df['img_size']).plot.hist(
+                bins=[bin_width*(x+0.5)+x_start for x in np.arange(0,n_bins)], 
+                edgecolor='black', 
+                color='#88d498')
+        print('true_box_size_frac max: ' 
+            + str((df['true_box_size']/df['img_size']).max()) 
+            + ', min: ' + str((df['true_box_size']/df['img_size']).min()))
         true_box_size_frac_plt.set_title('True Box Size as a Fraction of Total Image Size')
         true_box_size_frac_plt.get_figure().savefig(output_dir + 'true_box_size_frac_hist.png')
         true_box_size_frac_plt.get_figure().clf()
